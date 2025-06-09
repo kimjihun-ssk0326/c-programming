416bac89d4562d9a8ac8f04b9c4b538fe9097024
kimjihun-ssk0326 <kjh030326@gmail.com>
2025-06-09 17:13:35 +0900

    6/9 얼굴

---
+++ C프 게임/FileName.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("   _______\n");
+    printf("  /       \\\n");
+    printf(" |  O   O  |\n");
+    printf(" |    ^    |\n");
+    printf(" |  \\___/  |\n");
+    printf("  \\_______/\n");
+
+
+    printf("      ______\n");
+    printf("     /      \\\n");
+    printf("    |      O O\n");
+    printf("    |       > )\n");
+    printf("    |     \\___/\n");
+    printf("     \\______/\n");
+
+    printf("     ______\n");
+    printf("    /      \\\n");
+    printf("   O O      |\n");
+    printf("  ( <       |\n");
+    printf("   \\___/    |\n");
+    printf("    \\______/\n");
+
+    printf("   _______\n");
+    printf("  /  O O  \\\n");
+    printf(" |    ^    |\n");
+    printf(" |  \\___/  |\n");  // 입을 코 바로 아래로 올림
+    printf(" |         |\n");  // 빈 줄 하나 아래로 내림
+    printf("  \\_______/\n");
+
+
+    printf("  _______ \n");
+    printf(" /       \\\n");
+    printf("|         |\n");
+    printf("|         |\n");
+    printf("|    O  O  )\n");
+    printf("|      ^  |\n");
+    printf(" \\____'__/ \n");
+
+
+
+    return 0;
+
+
+}
